9cb71f9c1547ec18b55c8ef92b406809d0025877
pavanpatil <thepavanpatil.official@gmail.com>
2023-07-30 17:06:15 +0530

    day 11 commit

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/Models/catlog.dart';
+import 'package:untitled/widget/myDrawer.dart';
 
-import '../widget/myDrawer.dart';
+import '../widget/ItemWidget.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
+  final int days = 30;
+  final String name = "Codepur";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
-
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("This is the First App"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
-
-    drawer: myDrawer(),);
+      drawer: myDrawer(),
+    );
   }
 }
